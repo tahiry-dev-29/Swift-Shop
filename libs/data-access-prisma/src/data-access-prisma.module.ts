9248b9c6856996b30bb5be5f25ea881a1509722b
Tanner tahiry <tahirydev29@gmail.com>
2025-12-08 12:50:12 +0300

    feat: integrate Prisma 7 with PostgreSQL adapter and refactor API application structure.

--- libs/data-access-prisma/src/data-access-prisma.module.ts
+++ libs/data-access-prisma/src/data-access-prisma.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './lib/prisma.service';
 
 @Module({
   providers: [PrismaService],
